0189b9ee547a3278dd532c930e2ee4f5377fda0a
yeesonmin <55417940+yeesonmin@users.noreply.github.com>
2021-11-24 22:38:29 +0900

    Add Data Trigger

---
+++ CS WPF/03_Data_Trigger/ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Data_Trigger
+{
+    public class ViewModel
+    {
+        public int sldValue { get; set; }
+    }
+}
