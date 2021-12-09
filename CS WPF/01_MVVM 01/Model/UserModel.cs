64ad0edc177ce35593c204c3bb5cd81b1ea25e82
yeesonmin <55417940+yeesonmin@users.noreply.github.com>
2021-12-09 23:54:15 +0900

    Add Review MVVM

---
+++ CS WPF/01_MVVM 01/Model/UserModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MVVM_01.Model
+{
+    class UserModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                age = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string v = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(v));
+            }
+        }
+
+    }
+}
---
+++ CS WPF/01_MVVM 01/ViewModel/UserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using _01_MVVM_01.Command;
+using _01_MVVM_01.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MVVM_01.ViewModel
+{
+    class UserViewModel : ObservableCollection<UserModel>
+    {
+        RelayCommand AddUserCommand { get; set; }
+        public UserViewModel()
+        {
+            Add(new UserModel() { Name = "이선민", Age = 24 });
+            AddUserCommand = new RelayCommand(AddUser);
+        }
+
+        private void AddUser(object obj)
+        {
+            Add(new UserModel() { Name = obj.ToString(), Age = "new" });
+        }
+    }
+}
