ddbf91d1d4bc6904b3287f2e5b83106cc6ee1036
yeesonmin <yeesonmin@naver.com>
2022-01-18 22:36:08 +0900

    OpenCV 사칙연산, 논리연산

--- OpenCV/OpenCV01/OpenCV01/10_히스토그램.cpp
+++ OpenCV/OpenCV01/OpenCV01/10_히스토그램.cpp
@@ -1,98 +1,98 @@
-#include "opencv2/opencv.hpp"
-#include <iostream>
-
-using namespace cv;
-using namespace std;
-
-Mat calcGrayHist(const Mat& img);
-Mat getGrayHistImage(const Mat& hist);
-
-void histgoram_stretching();
-void histgoram_equalization();
-
-int main(void)
-{
-	histgoram_stretching();
-	histgoram_equalization();
-
-	return 0;
-}
-
-Mat calcGrayHist(const Mat& img)
-{
-	CV_Assert(img.type() == CV_8UC1);
-
-	Mat hist;
-	int channels[] = { 0 };
-	int dims = 1;//차원
-	const int histSize[] = { 256 };
-	float graylevel[] = { 0, 256 };
-	const float* ranges[] = { graylevel };
-
-	calcHist(&img, 1, channels, noArray(), hist, dims, histSize, ranges);
-
-	return hist;
-}
-
-Mat getGrayHistImage(const Mat& hist)
-{
-	CV_Assert(hist.type() == CV_32FC1);
-	CV_Assert(hist.size() == Size(1, 256));
-
-	double histMax;
-	minMaxLoc(hist, 0, &histMax);
-
-	Mat imgHist(100, 256, CV_8UC1, Scalar(255));
-	for (int i = 0; i < 256; i++) {
-		line(imgHist, Point(i, 100),
-			Point(i, 100 - cvRound(hist.at<float>(i, 0) * 100 / histMax)), Scalar(0));
-	}
-
-	return imgHist;
-}
-
-void histgoram_stretching()
-{
-	Mat src = imread("hawkes.bmp", IMREAD_GRAYSCALE);
-
-	if (src.empty()) {
-		cerr << "Image load failed!" << endl;
-		return;
-	}
-
-	double gmin, gmax;
-	minMaxLoc(src, &gmin, &gmax);//최소 및 최댓값 구하기
-
-	Mat dst = (src - gmin) * 255 / (gmax - gmin);//히스토그램 스트레칭 수식
-
-	imshow("src", src);
-	imshow("srcHist", getGrayHistImage(calcGrayHist(src)));
-
-	imshow("dst", dst);
-	imshow("dstHist", getGrayHistImage(calcGrayHist(dst)));
-
-	waitKey();
-	destroyAllWindows();
-}
-
-void histgoram_equalization()
-{
-	Mat src = imread("hawkes.bmp", IMREAD_GRAYSCALE);
-
-	if (src.empty()) {
-		cerr << "Image load failed!" << endl;
-		return;
-	}
-
-	Mat dst;
-	equalizeHist(src, dst);//히스토그램 평탄화
-
-	imshow("src", src);
-	imshow("srcHist", getGrayHistImage(calcGrayHist(src)));
-
-	imshow("dst", dst);
-	imshow("dstHist", getGrayHistImage(calcGrayHist(dst)));
-
-	waitKey();
-	destroyAllWindows();
-}
+//#include "opencv2/opencv.hpp"
+//#include <iostream>
+//
+//using namespace cv;
+//using namespace std;
+//
+//Mat calcGrayHist(const Mat& img);
+//Mat getGrayHistImage(const Mat& hist);
+//
+//void histgoram_stretching();
+//void histgoram_equalization();
+//
+//int main(void)
+//{
+//	histgoram_stretching();
+//	histgoram_equalization();
+//
+//	return 0;
+//}
+//
+//Mat calcGrayHist(const Mat& img)
+//{
+//	CV_Assert(img.type() == CV_8UC1);
+//
+//	Mat hist;
+//	int channels[] = { 0 };
+//	int dims = 1;//차원
+//	const int histSize[] = { 256 };
+//	float graylevel[] = { 0, 256 };
+//	const float* ranges[] = { graylevel };
+//
+//	calcHist(&img, 1, channels, noArray(), hist, dims, histSize, ranges);
+//
+//	return hist;
+//}
+//
+//Mat getGrayHistImage(const Mat& hist)
+//{
+//	CV_Assert(hist.type() == CV_32FC1);
+//	CV_Assert(hist.size() == Size(1, 256));
+//
+//	double histMax;
+//	minMaxLoc(hist, 0, &histMax);
+//
+//	Mat imgHist(100, 256, CV_8UC1, Scalar(255));
+//	for (int i = 0; i < 256; i++) {
+//		line(imgHist, Point(i, 100),
+//			Point(i, 100 - cvRound(hist.at<float>(i, 0) * 100 / histMax)), Scalar(0));
+//	}
+//
+//	return imgHist;
+//}
+//
+//void histgoram_stretching()
+//{
+//	Mat src = imread("hawkes.bmp", IMREAD_GRAYSCALE);
+//
+//	if (src.empty()) {
+//		cerr << "Image load failed!" << endl;
+//		return;
+//	}
+//
+//	double gmin, gmax;
+//	minMaxLoc(src, &gmin, &gmax);//최소 및 최댓값 구하기
+//
+//	Mat dst = (src - gmin) * 255 / (gmax - gmin);//히스토그램 스트레칭 수식
+//
+//	imshow("src", src);
+//	imshow("srcHist", getGrayHistImage(calcGrayHist(src)));
+//
+//	imshow("dst", dst);
+//	imshow("dstHist", getGrayHistImage(calcGrayHist(dst)));
+//
+//	waitKey();
+//	destroyAllWindows();
+//}
+//
+//void histgoram_equalization()
+//{
+//	Mat src = imread("hawkes.bmp", IMREAD_GRAYSCALE);
+//
+//	if (src.empty()) {
+//		cerr << "Image load failed!" << endl;
+//		return;
+//	}
+//
+//	Mat dst;
+//	equalizeHist(src, dst);//히스토그램 평탄화
+//
+//	imshow("src", src);
+//	imshow("srcHist", getGrayHistImage(calcGrayHist(src)));
+//
+//	imshow("dst", dst);
+//	imshow("dstHist", getGrayHistImage(calcGrayHist(dst)));
+//
+//	waitKey();
+//	destroyAllWindows();
+//}
