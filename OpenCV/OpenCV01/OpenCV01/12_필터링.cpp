af449c4b9eea38448e7643942f971a47f4eab943
yeesonmin <yeesonmin@naver.com>
2022-02-03 20:36:20 +0900

    openCV 기하학적 변환

--- OpenCV/OpenCV01/OpenCV01/12_필터링.cpp
+++ OpenCV/OpenCV01/OpenCV01/12_필터링.cpp
@@ -1,217 +1,217 @@
-#include "opencv2/opencv.hpp"
-#include <iostream>
-
-using namespace cv;
-using namespace std;
-
-void filter_embossing();//엠보싱 효과
-void blurring_mean(); //초점 흐림
-void blurring_gaussian();//초점 흐림 부드러운 버전
-void unsharp_mask();//좀 더 선명하게
-
-void noise_gaussian();
-void filter_bilateral();
-void filter_median();
-
-int main(void)
-{
-	//filter_embossing();
-	//blurring_mean();
-	//blurring_gaussian();
-	//unsharp_mask();
-	noise_gaussian();
-	filter_bilateral();
-	filter_median();
-
-	return 0;
-}
-
-void filter_embossing()
-{
-	Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
-
-	if (src.empty()) {
-		cerr << "Image load failed!" << endl;
-		return;
-	}
-
-	float data[] = { -1, -1, 0, -1, 0, 1, 0, 1, 1 };
-	Mat emboss(3, 3, CV_32FC1, data);
-
-	Mat dst, dst2;
-	filter2D(src, dst, -1, emboss, Point(-1, -1), 128);
-	filter2D(src, dst2, -1, emboss, Point(-1, -1), 0);
-
-	imshow("src", src);
-	imshow("dst", dst);
-	imshow("dst2", dst2);
-
-	waitKey();
-	destroyAllWindows();
-}
-
-void blurring_mean()
-{
-	Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
-
-	if (src.empty()) {
-		cerr << "Image load failed!" << endl;
-		return;
-	}
-
-	imshow("src", src);
-
-	Mat dst;
-	for (int ksize = 3; ksize <= 7; ksize += 2) {
-		blur(src, dst, Size(ksize, ksize));//Size(ksize, ksize) => 값이 커질 수록 흐려짐
-
-		String desc = format("Mean: %dx%d", ksize, ksize);
-		putText(dst, desc, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0,
-			Scalar(255), 1, LINE_AA);
-
-		imshow("dst", dst);
-		waitKey();
-	}
-
-	destroyAllWindows();
-}
-
-void blurring_gaussian()
-{
-	Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
-
-	if (src.empty()) {
-		cerr << "Image load failed!" << endl;
-		return;
-	}
-
-	imshow("src", src);
-
-	Mat dst;
-	for (int sigma = 1; sigma <= 5; sigma++) {
-		GaussianBlur(src, dst, Size(0, 0), (double)sigma);
-
-		String desc = format("Gaussian: sigma = %d", sigma);
-		putText(dst, desc, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0,
-			Scalar(255), 1, LINE_AA);
-
-		imshow("dst", dst);
-		waitKey();
-	}
-
-	destroyAllWindows();
-}
-
-void unsharp_mask()
-{
-	Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
-
-	if (src.empty()) {
-		cerr << "Image load failed!" << endl;
-		return;
-	}
-
-	imshow("src", src);
-
-	for (int sigma = 1; sigma <= 5; sigma++) {
-		Mat blurred;
-		GaussianBlur(src, blurred, Size(), sigma);
-
-		float alpha = 1.f;
-		Mat dst = (1 + alpha) * src - alpha * blurred;//언샤프닝 마스크 공식
-
-		String desc = format("sigma: %d", sigma);
-		putText(dst, desc, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0,
-			Scalar(255), 1, LINE_AA);
-
-		imshow("dst", dst);
-		waitKey();
-	}
-
-	destroyAllWindows();
-}
-
-
-void noise_gaussian()
-{
-	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
-
-	if (src.empty()) {
-		cerr << "Image load failed!" << endl;
-		return;
-	}
-
-	imshow("src", src);
-
-	for (int stddev = 10; stddev <= 30; stddev += 10) {
-		Mat noise(src.size(), CV_32SC1);
-		randn(noise, 0, stddev);
-
-		Mat dst;
-		add(src, noise, dst, Mat(), CV_8U);
-
-		String desc = format("stddev = %d", stddev);
-		putText(dst, desc, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255), 1, LINE_AA);
-		imshow("dst", dst);
-		waitKey();
-	}
-
-	destroyAllWindows();
-}
-
-void filter_bilateral()
-{
-	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
-
-	if (src.empty()) {
-		cerr << "Image load failed!" << endl;
-		return;
-	}
-
-	Mat noise(src.size(), CV_32SC1);
-	randn(noise, 0, 5);
-	add(src, noise, src, Mat(), CV_8U);
-
-	Mat dst1;
-	GaussianBlur(src, dst1, Size(), 5);
-
-	Mat dst2;
-	bilateralFilter(src, dst2, -1, 10, 5);//색 공간 표준편차 10, 좌표 표준편차 5
-
-	imshow("src", src);
-	imshow("dst1", dst1);
-	imshow("dst2", dst2);
-
-	waitKey();
-	destroyAllWindows();
-}
-
-void filter_median()
-{
-	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
-
-	if (src.empty()) {
-		cerr << "Image load failed!" << endl;
-		return;
-	}
-
-	int num = (int)(src.total() * 0.1);
-	for (int i = 0; i < num; i++) {
-		int x = rand() % src.cols;
-		int y = rand() % src.rows;
-		src.at<uchar>(y, x) = (i % 2) * 255;
-	}//잡음 생성
-
-	Mat dst1;
-	GaussianBlur(src, dst1, Size(), 1);
-
-	Mat dst2;
-	medianBlur(src, dst2, 3);
-
-	imshow("src", src);
-	imshow("dst1", dst1);
-	imshow("dst2", dst2);
-
-	waitKey();
-	destroyAllWindows();
-}
+//#include "opencv2/opencv.hpp"
+//#include <iostream>
+//
+//using namespace cv;
+//using namespace std;
+//
+//void filter_embossing();//엠보싱 효과
+//void blurring_mean(); //초점 흐림
+//void blurring_gaussian();//초점 흐림 부드러운 버전
+//void unsharp_mask();//좀 더 선명하게
+//
+//void noise_gaussian();
+//void filter_bilateral();
+//void filter_median();
+//
+//int main(void)
+//{
+//	//filter_embossing();
+//	//blurring_mean();
+//	//blurring_gaussian();
+//	//unsharp_mask();
+//	noise_gaussian();
+//	filter_bilateral();
+//	filter_median();
+//
+//	return 0;
+//}
+//
+//void filter_embossing()
+//{
+//	Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
+//
+//	if (src.empty()) {
+//		cerr << "Image load failed!" << endl;
+//		return;
+//	}
+//
+//	float data[] = { -1, -1, 0, -1, 0, 1, 0, 1, 1 };
+//	Mat emboss(3, 3, CV_32FC1, data);
+//
+//	Mat dst, dst2;
+//	filter2D(src, dst, -1, emboss, Point(-1, -1), 128);
+//	filter2D(src, dst2, -1, emboss, Point(-1, -1), 0);
+//
+//	imshow("src", src);
+//	imshow("dst", dst);
+//	imshow("dst2", dst2);
+//
+//	waitKey();
+//	destroyAllWindows();
+//}
+//
+//void blurring_mean()
+//{
+//	Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
+//
+//	if (src.empty()) {
+//		cerr << "Image load failed!" << endl;
+//		return;
+//	}
+//
+//	imshow("src", src);
+//
+//	Mat dst;
+//	for (int ksize = 3; ksize <= 7; ksize += 2) {
+//		blur(src, dst, Size(ksize, ksize));//Size(ksize, ksize) => 값이 커질 수록 흐려짐
+//
+//		String desc = format("Mean: %dx%d", ksize, ksize);
+//		putText(dst, desc, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0,
+//			Scalar(255), 1, LINE_AA);
+//
+//		imshow("dst", dst);
+//		waitKey();
+//	}
+//
+//	destroyAllWindows();
+//}
+//
+//void blurring_gaussian()
+//{
+//	Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
+//
+//	if (src.empty()) {
+//		cerr << "Image load failed!" << endl;
+//		return;
+//	}
+//
+//	imshow("src", src);
+//
+//	Mat dst;
+//	for (int sigma = 1; sigma <= 5; sigma++) {
+//		GaussianBlur(src, dst, Size(0, 0), (double)sigma);
+//
+//		String desc = format("Gaussian: sigma = %d", sigma);
+//		putText(dst, desc, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0,
+//			Scalar(255), 1, LINE_AA);
+//
+//		imshow("dst", dst);
+//		waitKey();
+//	}
+//
+//	destroyAllWindows();
+//}
+//
+//void unsharp_mask()
+//{
+//	Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
+//
+//	if (src.empty()) {
+//		cerr << "Image load failed!" << endl;
+//		return;
+//	}
+//
+//	imshow("src", src);
+//
+//	for (int sigma = 1; sigma <= 5; sigma++) {
+//		Mat blurred;
+//		GaussianBlur(src, blurred, Size(), sigma);
+//
+//		float alpha = 1.f;
+//		Mat dst = (1 + alpha) * src - alpha * blurred;//언샤프닝 마스크 공식
+//
+//		String desc = format("sigma: %d", sigma);
+//		putText(dst, desc, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0,
+//			Scalar(255), 1, LINE_AA);
+//
+//		imshow("dst", dst);
+//		waitKey();
+//	}
+//
+//	destroyAllWindows();
+//}
+//
+//
+//void noise_gaussian()
+//{
+//	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+//
+//	if (src.empty()) {
+//		cerr << "Image load failed!" << endl;
+//		return;
+//	}
+//
+//	imshow("src", src);
+//
+//	for (int stddev = 10; stddev <= 30; stddev += 10) {
+//		Mat noise(src.size(), CV_32SC1);
+//		randn(noise, 0, stddev);
+//
+//		Mat dst;
+//		add(src, noise, dst, Mat(), CV_8U);
+//
+//		String desc = format("stddev = %d", stddev);
+//		putText(dst, desc, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255), 1, LINE_AA);
+//		imshow("dst", dst);
+//		waitKey();
+//	}
+//
+//	destroyAllWindows();
+//}
+//
+//void filter_bilateral()
+//{
+//	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+//
+//	if (src.empty()) {
+//		cerr << "Image load failed!" << endl;
+//		return;
+//	}
+//
+//	Mat noise(src.size(), CV_32SC1);
+//	randn(noise, 0, 5);
+//	add(src, noise, src, Mat(), CV_8U);
+//
+//	Mat dst1;
+//	GaussianBlur(src, dst1, Size(), 5);
+//
+//	Mat dst2;
+//	bilateralFilter(src, dst2, -1, 10, 5);//색 공간 표준편차 10, 좌표 표준편차 5
+//
+//	imshow("src", src);
+//	imshow("dst1", dst1);
+//	imshow("dst2", dst2);
+//
+//	waitKey();
+//	destroyAllWindows();
+//}
+//
+//void filter_median()
+//{
+//	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+//
+//	if (src.empty()) {
+//		cerr << "Image load failed!" << endl;
+//		return;
+//	}
+//
+//	int num = (int)(src.total() * 0.1);
+//	for (int i = 0; i < num; i++) {
+//		int x = rand() % src.cols;
+//		int y = rand() % src.rows;
+//		src.at<uchar>(y, x) = (i % 2) * 255;
+//	}//잡음 생성
+//
+//	Mat dst1;
+//	GaussianBlur(src, dst1, Size(), 1);
+//
+//	Mat dst2;
+//	medianBlur(src, dst2, 3);
+//
+//	imshow("src", src);
+//	imshow("dst1", dst1);
+//	imshow("dst2", dst2);
+//
+//	waitKey();
+//	destroyAllWindows();
+//}
