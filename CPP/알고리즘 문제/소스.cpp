02345c0982e3b15103abec4767019855d2cf949a
yeesonmin <yeesonmin@naver.com>
2022-03-25 22:26:55 +0900

    CPP 알고리즘 문제 DFS

--- CPP/알고리즘 문제/소스.cpp
+++ CPP/알고리즘 문제/소스.cpp
@@ -1,49 +1,52 @@
 #include <iostream>
-#include <string>
+#include <vector>
 using namespace std;
 
+
+vector<int> graph[8];
+bool visited[8] = { false };
+
+void DFS(int i) {
+	cout << i+1 << " ";
+	visited[i] = true;
+
+	for (int j : graph[i])
+	{
+		if (!visited[j-1]) {
+			DFS(j-1);
+		}
+	}
+}
 void main() {
-	//¿Õ½ÇÀÇ ³ªÀÌÆ®
-	string s;
-	cin >> s;
+	//DFS
+	
 
-	//a~h
-	//1~8
-	int row = s[0] - 'a' + 1;
-	int col = s[1] - '0';
-	int result = 0;
+	graph[0].push_back(2);
+	graph[0].push_back(3);
+	graph[0].push_back(8);
 
-	//¿ì2
-	if (row + 2 <= 8) {
-		//À§1
-		if (col - 1 > 0) result++;
-		//¾Æ1
-		if (col + 1 <= 8) result++;
-	}
+	graph[1].push_back(1);
+	graph[1].push_back(7);
 
-	//ÁÂ2
-	if (row - 2 > 0) {
-		//À§1
-		if (col - 1 > 0) result++;
-		//¾Æ1
-		if (col + 1 <= 8) result++;
-	}
+	graph[2].push_back(1);
+	graph[2].push_back(4);
+	graph[2].push_back(5);
 
-	//»ó2
-	if (col - 2 > 0) {
-		//¿Þ1
-		if (row - 1 > 0) result++;
-		//¿ì1
-		if (row + 1 <= 8) result++;
-	}
+	graph[3].push_back(3);
+	graph[3].push_back(5);
 
-	//ÇÏ2
-	if (col + 2 <= 8) {
-		//¿Þ1
-		if (row - 1 > 0) result++;
-		//¿ì1
-		if (row + 1 <= 8) result++;
-	}
+	graph[4].push_back(3);
+	graph[4].push_back(4);
 
-	cout << result;
+	graph[5].push_back(7);
+
+	graph[6].push_back(2);
+	graph[6].push_back(6);
+	graph[6].push_back(8);
+
+	graph[7].push_back(1);
+	graph[7].push_back(7);
+
+	DFS(0);
+
 }
